fd54bcd33d2d2c7bcd90102440a4a936589f7be9
uflores04 <urias.flores@codexitos.com>
2026-02-02 22:08:16 -0600

    chore: improve deployment using ecosystem files to run application on pm2 directly

---
+++ nextvisa-api/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [{
+    name: "nextvisa-api",
+    script: "./.venv/bin/gunicorn",
+    args: "main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000",
+    interpreter: "python3",
+    env_file: ".env",
+    cwd: "/home/nextvisa/nextvisa-api",
+    autorestart: true,
+    watch: false
+  }]
+}
