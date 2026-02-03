5c4ab82e887f225178633135bc17e9aa2bc0edff
uflores04 <urias.flores@codexitos.com>
2026-02-02 23:41:45 -0600

    fix: resolve dir

--- nextvisa-api/ecosystem.config.js
+++ nextvisa-api/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "nextvisa-api",
     script: "./.venv/bin/gunicorn",
-    args: "main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000",
+    args: "main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:10400",
     interpreter: "python3",
     env_file: ".env",
     cwd: "/home/nextvisa/nextvisa-api",
