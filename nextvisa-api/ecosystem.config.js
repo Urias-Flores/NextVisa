97119714560df5c2af2cdf33e403dc7fbc8241c1
uflores04 <urias.flores@codexitos.com>
2026-02-21 12:56:55 -0600

    fix: guvnicorn module not found when start project

--- nextvisa-api/ecosystem.config.js
+++ nextvisa-api/ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [{
     name: "nextvisa-api",
-    script: ".venv/bin/gunicorn",
+    script: "/home/nextvisa/nextvisa-api/.venv/bin/gunicorn",
     args: "main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:10400",
-    interpreter: "python3",
+    interpreter: "none",
     env_file: ".env",
     cwd: "/home/nextvisa/nextvisa-api",
     autorestart: true,
